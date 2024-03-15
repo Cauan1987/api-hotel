c451a0c96082a337a73f941363ce27da549bbc26
Cauan1987 <128874719+Cauan1987@users.noreply.github.com>
2024-03-15 15:14:57 -0300

    adicionei duas rotas

--- src/models/conexao.js
+++ src/models/conexao.js
@@ -1,10 +1,12 @@
-import { createConnection } from "mysql";
+import mysql from "mysql";
 
-export const conexao = createConnection({
+const conexao = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
 });
+
+export default conexao
 
 
